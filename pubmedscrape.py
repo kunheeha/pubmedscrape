03ad52b38330d07460e53334282a6e4be5b56072
Kunhee Ha <kunheeha@gmail.com>
2021-09-09 19:26:25 +0100

    added comments

--- pubmedscrape.py
+++ pubmedscrape.py
@@ -1,10 +1,14 @@
-# https://pubmed.ncbi.nlm.nih.gov/?term=search+words
+# Scraping from PubMed - database of biomedical literature
+# https://pubmed.ncbi.nlm.nih.gov/?term=example+search+words
 import requests
 from bs4 import BeautifulSoup
 
+# Take User Input of Search keywords
 keywords = input('Search: ')
+# Create txt file named after search keywords
 results_file = open(f'{keywords}.txt', 'x')
 results_file.close()
+# formatting search keywords to 'exmaple+search+words' 
 keyword_list = keywords.split()
 search = ""
 for i in range(len(keyword_list)-1):
@@ -12,6 +16,7 @@ for i in range(len(keyword_list)-1):
     search += '+'
 search += keyword_list[-1]
 
+# Scraping all links to first 10 articles
 source = requests.get(f'https://pubmed.ncbi.nlm.nih.gov/?term={search}').text
 soup = BeautifulSoup(source, 'lxml')
 articles = soup.find_all('article', class_='full-docsum')
@@ -19,6 +24,7 @@ article_links=[]
 for article in articles:
     article_links.append(article.a.get('href'))
 
+# Scraping title, abstract, links to full text from links above
 def article_details(link):
     source = requests.get(f'https://pubmed.ncbi.nlm.nih.gov/{link}').text
     soup = BeautifulSoup(source, 'lxml')
@@ -27,6 +33,7 @@ def article_details(link):
     full_text_links = soup.find('div', class_='full-text-links-list')
     for link in full_text_links.find_all('a'):
         full_texts_list.append(link.get('href'))
+    # Try Except because some may not have an abstract
     try:
         abstract_div = soup.find('div', class_='abstract-content')
         abstract = abstract_div.p.get_text()
@@ -48,6 +55,7 @@ def article_details(link):
 
     return details 
 
+# Store results in txt file created at start
 results_file = open(f'{keywords}.txt', 'a')
 for link in article_links:
     results_file.write(article_details(link))
